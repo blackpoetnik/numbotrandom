d7a117f2271ce7c652ce9914aaa1a4040cb5fb91
blackpoetnik <63372705+blackpoetnik@users.noreply.github.com>
2020-04-21 21:46:59 +0300

    Update numbot.py

--- numbot.py
+++ numbot.py
@@ -29,14 +29,3 @@ def start_message(message):
 
 #я надеюсь, ты без коментариев понимаешь, что это тупа угадайка, от 0 до 9)))
 
-
-
-
-heroku login blackpoetnik@gmail.com Slognone1337.
-heroku create --region eu numbotrandom #Не забываемпоменять имя приложения
-#P.S. в имени могут быть только буквы в нижнем регитсре, цифры  и тире.
-heroku addons:create heroku-redis:hobby-dev -a numbotrandom #И снова меняем имя!
-heroku buildpacks:set heroku/python
-git push heroku master
-heroku ps:scale bot=1 # запускаем бота
-heroku logs --tail #включаем логи
